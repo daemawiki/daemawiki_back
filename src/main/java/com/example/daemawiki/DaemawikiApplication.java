19df5e0fe7f52f3208685bac8b3028b2bb64ba32
101 <tmddnjs001133@dsm.hs.kr>
2024-01-19 14:01:04 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/daemawiki/DaemawikiApplication.java
@@ -0,0 +1,13 @@
+package com.example.daemawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaemawikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaemawikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/daemawiki/DaemawikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daemawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemawikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
