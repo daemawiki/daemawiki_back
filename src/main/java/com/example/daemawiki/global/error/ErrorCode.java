649a425252be0bb5d8018af8e5b5057d4d86c55e
101 <tmddnjs001133@dsm.hs.kr>
2024-01-22 14:19:29 +0900

    file setting

--- src/main/java/com/example/daemawiki/global/error/ErrorCode.java
+++ src/main/java/com/example/daemawiki/global/error/ErrorCode.java
@@ -1,4 +1,14 @@
 package com.example.daemawiki.global.error;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ErrorCode {
+
+    TEST(400, "TEST");
+
+    private final int httpStatus;
+    private final String message;
 }
