eed58c6a6d6edf3d05d4cb8b81e41481a8a700ab
101 <tmddnjs001133@dsm.hs.kr>
2024-01-23 16:16:33 +0900

    web filter 추가

---
+++ src/main/java/com/example/daemawiki/global/security/JwtWebFilter.java
@@ -0,0 +1,39 @@
+package com.example.daemawiki.global.security;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+public class JwtWebFilter implements WebFilter {
+    private final Tokenizer tokenizer;
+
+    public JwtWebFilter(Tokenizer tokenizer) {
+        this.tokenizer = tokenizer;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return resolveToken(exchange.getRequest())
+                .flatMap(token -> {
+                    if (tokenizer.verify(token)) {
+                        Authentication authentication = tokenizer.getAuthentication(token);
+                        return chain.filter(exchange)
+                                .contextWrite(ReactiveSecurityContextHolder.withAuthentication(authentication));
+                    } else {
+                        return chain.filter(exchange);
+                    }
+                });
+    }
+
+    private Mono<String> resolveToken(ServerHttpRequest request) {
+        return Mono.justOrEmpty(request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION))
+                .filter(authHeader -> authHeader.startsWith("Bearer "))
+                .map(authHeader -> authHeader.substring(7));
+    }
+
+}
