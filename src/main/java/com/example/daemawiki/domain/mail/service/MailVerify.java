b9269d563603805e535ef569a96d1dd8435428f8
101 <tmddnjs001133@dsm.hs.kr>
2024-01-25 00:13:50 +0900

    메일 인증 로직 구현

---
+++ src/main/java/com/example/daemawiki/domain/mail/service/MailVerify.java
@@ -0,0 +1,35 @@
+package com.example.daemawiki.domain.mail.service;
+
+import com.example.daemawiki.domain.mail.dto.AuthCodeVerifyRequest;
+import com.example.daemawiki.domain.mail.model.AuthCode;
+import com.example.daemawiki.domain.mail.model.AuthMail;
+import com.example.daemawiki.domain.mail.repository.AuthCodeRepository;
+import com.example.daemawiki.domain.mail.repository.AuthMailRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class MailVerify {
+    private final AuthMailRepository mailRepository;
+    private final AuthCodeRepository codeRepository;
+
+    public MailVerify(AuthMailRepository authMailRepository, AuthCodeRepository authCodeRepository) {
+        this.mailRepository = authMailRepository;
+        this.codeRepository = authCodeRepository;
+    }
+
+    public Mono<Boolean> verify(AuthCodeVerifyRequest request) {
+        return getAuthCode(request.mail(), request.authCode())
+                .flatMap(authCode -> mailRepository.save(AuthMail.builder()
+                                .mail(request.mail())
+                                .build())
+                        .then(codeRepository.delete(authCode))
+                        .thenReturn(true))
+                .switchIfEmpty(Mono.just(false));
+    }
+
+    private Mono<AuthCode> getAuthCode(String mail, String authCode) {
+        return codeRepository.findByMailAndCode(mail, authCode);
+    }
+
+}
