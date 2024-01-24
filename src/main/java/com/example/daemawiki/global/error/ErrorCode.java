16a0dd67fa56034ce0f17a9f4ae179b10a7ffc64
101 <tmddnjs001133@dsm.hs.kr>
2024-01-25 02:04:57 +0900

    exception 추가

--- src/main/java/com/example/daemawiki/global/error/ErrorCode.java
+++ src/main/java/com/example/daemawiki/global/error/ErrorCode.java
@@ -9,6 +9,9 @@ public enum ErrorCode {
 
     PASSWORD_MISMATCH(401, "비밀번호가 일치하지 않습니다."),
 
+    MAIL_CONFIRM_FAILED(500, "메일 전송 실패 (서버 오류)"),
+
+
     TEST(400, "TEST");
 
     private final int httpStatus;
---
+++ src/main/java/com/example/daemawiki/global/exception/MailSendFailedException.java
@@ -0,0 +1,13 @@
+package com.example.daemawiki.global.exception;
+
+import com.example.daemawiki.global.error.ErrorCode;
+import com.example.daemawiki.global.error.exception.CustomException;
+
+public class MailSendFailedException extends CustomException {
+    public static final CustomException EXCEPTION = new MailSendFailedException();
+
+    private MailSendFailedException() {
+        super(ErrorCode.MAIL_CONFIRM_FAILED);
+    }
+
+}
