aee26dcfbc1247ead96a1f51822b511c9275d733
101 <tmddnjs001133@dsm.hs.kr>
2024-01-24 13:42:14 +0900

    User model 추가

---
+++ src/main/java/com/example/daemawiki/domain/user/model/User.java
@@ -0,0 +1,30 @@
+package com.example.daemawiki.domain.user.model;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Document
+@NoArgsConstructor
+public class User {
+
+    @Id
+    private String id;
+
+    private String nickname;
+
+    private String email;
+
+    private String password;
+
+    @Builder
+    public User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
+
+}
