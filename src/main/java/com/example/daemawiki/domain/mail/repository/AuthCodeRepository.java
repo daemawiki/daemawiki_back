8e92d315772206febd769345a22df47faa23bd9a
101 <tmddnjs001133@dsm.hs.kr>
2024-01-25 20:27:37 +0900

    reactive repository 삭제
    레디스가 리액티브 레포지토리 지원 안 하는 걸 몰랏지;;

--- src/main/java/com/example/daemawiki/domain/mail/repository/AuthCodeRepository.java
+++ src/main/java/com/example/daemawiki/domain/mail/repository/AuthCodeRepository.java
@@ -1,10 +1,54 @@
 package com.example.daemawiki.domain.mail.repository;
 
 import com.example.daemawiki.domain.mail.model.AuthCode;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import com.example.daemawiki.domain.mail.model.AuthMail;
+import com.example.daemawiki.domain.mail.type.RedisKey;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
-public interface AuthCodeRepository extends ReactiveCrudRepository<AuthCode, String> {
-    Mono<AuthCode> findByMailAndCode(String mail, String code);
-    Mono<AuthCode> findByMail(String mail);
+import java.time.Duration;
+
+@Repository
+public class AuthCodeRepository {
+    private final ReactiveRedisOperations<String, String> redisOperations;
+
+    public AuthCodeRepository(ReactiveRedisOperations<String, String> redisOperations) {
+        this.redisOperations = redisOperations;
+    }
+
+    private static final String AUTHCODE = RedisKey.AUTH_CODE.getKey();
+
+    public Mono<AuthCode> findByMail(String mail) {
+        return redisOperations.opsForValue().get(AUTHCODE + mail)
+                .map(value -> AuthCode.builder()
+                        .mail(mail)
+                        .code(value).build());
+    }
+
+    public Mono<AuthCode> delete(AuthCode authCode) {
+        return redisOperations.delete(AUTHCODE + authCode.getMail())
+                .map(deleted -> {
+                    if (deleted == 1) {
+                        return authCode;
+                    }
+                    return null;
+                });
+    }
+
+    public Mono<AuthCode> save(AuthCode authCode) {
+        return redisOperations.opsForValue()
+                .set(AUTHCODE + authCode.getMail(),
+                        authCode.getCode(),
+                        Duration.ofHours(3))
+                .map(result -> {
+                    if (result) {
+                        return authCode;
+                    }
+                    return null;
+                });
+    }
+
+
 }
--- src/main/java/com/example/daemawiki/domain/mail/repository/AuthMailRepository.java
+++ src/main/java/com/example/daemawiki/domain/mail/repository/AuthMailRepository.java
@@ -1,7 +1,32 @@
 package com.example.daemawiki.domain.mail.repository;
 
 import com.example.daemawiki.domain.mail.model.AuthMail;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import com.example.daemawiki.domain.mail.type.RedisKey;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
-public interface AuthMailRepository extends ReactiveCrudRepository<AuthMail, String> {
+import java.time.Duration;
+
+@Repository
+public class AuthMailRepository {
+
+    private final ReactiveRedisOperations<String, String> redisOperations;
+
+    @Autowired
+    public AuthMailRepository(ReactiveRedisOperations<String, String> redisOperations) {
+        this.redisOperations = redisOperations;
+    }
+
+    private static final String AUTHMAIL = RedisKey.AUTH_MAIL.getKey();
+
+    public Mono<Void> save(AuthMail authMail) {
+        return redisOperations.opsForValue()
+                .set(AUTHMAIL + authMail.getMail(),
+                        authMail.getMail(),
+                        Duration.ofHours(9))
+                .then();
+    }
+
 }
