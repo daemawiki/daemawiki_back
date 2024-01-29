ccf63a70a7ac94195692d344c3b9feffbad853f8
101 <tmddnjs001133@dsm.hs.kr>
2024-01-29 13:06:12 +0900

    회원가입 로직 추가

---
+++ src/main/java/com/example/daemawiki/domain/auth/service/Signup.java
@@ -0,0 +1,48 @@
+package com.example.daemawiki.domain.auth.service;
+
+import com.example.daemawiki.domain.auth.dto.SignupRequest;
+import com.example.daemawiki.domain.mail.repository.AuthMailRepository;
+import com.example.daemawiki.domain.user.model.User;
+import com.example.daemawiki.domain.user.repository.UserRepository;
+import com.example.daemawiki.global.exception.EmailAlreadyExistsException;
+import com.example.daemawiki.global.exception.UnVerifiedEmailException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+
+@Service
+public class Signup {
+    private final UserRepository userRepository;
+    private final AuthMailRepository authMailRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final Scheduler scheduler;
+
+    public Signup(UserRepository userRepository,AuthMailRepository authMailRepository, PasswordEncoder passwordEncoder, Scheduler scheduler) {
+        this.userRepository = userRepository;
+        this.authMailRepository = authMailRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.scheduler = scheduler;
+    }
+
+    public Mono<Void> execute(SignupRequest request) {
+        return userRepository.findByEmail(request.email())
+                .flatMap(user -> Mono.error(EmailAlreadyExistsException.EXCEPTION))
+                .switchIfEmpty(Mono.defer(() -> authMailRepository.findByMail(request.email())
+                            .flatMap(verified -> {
+                                if (!verified) {
+                                    return Mono.error(UnVerifiedEmailException.EXCEPTION);
+                                }
+
+                                return Mono.fromCallable(() -> passwordEncoder.encode(request.password()))
+                                        .subscribeOn(scheduler)
+                                        .map(password -> User.builder()
+                                                    .nickname(request.nickname())
+                                                    .email(request.email())
+                                                    .password(password)
+                                                    .build())
+                                        .flatMap(userRepository::save);
+                            }))).then();
+    }
+
+}
