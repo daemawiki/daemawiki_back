462210dd837227d87ff3c5733275ea03e28ede02
101 <tmddnjs001133@dsm.hs.kr>
2024-01-29 13:03:31 +0900

    find by mail 추가 (Mongo)

--- src/main/java/com/example/daemawiki/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/daemawiki/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.daemawiki.domain.user.repository;
 
 import com.example.daemawiki.domain.user.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    Mono<User> findByEmail(String email);
 }
