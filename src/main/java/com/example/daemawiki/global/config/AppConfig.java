f98f3b93908a18b4eab034183b5155bdcf3f30c9
101 <tmddnjs001133@dsm.hs.kr>
2024-01-29 13:05:57 +0900

    app config 추가
    스레드

---
+++ src/main/java/com/example/daemawiki/global/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.daemawiki.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public Scheduler scheduler() {
+        return Schedulers.boundedElastic();
+    }
+
+}
