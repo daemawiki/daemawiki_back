e8b423b3c814806babbae95cd5a0a9489f3cf9d4
101 <tmddnjs001133@dsm.hs.kr>
2024-01-29 13:05:03 +0900

    user not found exception 추가

---
+++ src/main/java/com/example/daemawiki/global/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.daemawiki.global.exception;
+
+import com.example.daemawiki.global.error.ErrorCode;
+import com.example.daemawiki.global.error.exception.CustomException;
+
+public class UserNotFoundException extends CustomException {
+    public static final CustomException EXCEPTION = new UserNotFoundException();
+
+    private UserNotFoundException() {
+        super(ErrorCode.USER_NOT_FOUND);
+    }
+
+}
