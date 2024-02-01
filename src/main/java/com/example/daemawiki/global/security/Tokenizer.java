ec45d4f08eaabd7eaafd86c627c1fae588251b3c
101 <tmddnjs001133@dsm.hs.kr>
2024-02-01 21:10:33 +0900

    dto package 변경

--- src/main/java/com/example/daemawiki/global/security/Tokenizer.java
+++ src/main/java/com/example/daemawiki/global/security/Tokenizer.java
@@ -1,7 +1,7 @@
 package com.example.daemawiki.global.security;
 
-import com.example.daemawiki.domain.auth.dto.ReissueRequest;
-import com.example.daemawiki.domain.auth.dto.TokenResponse;
+import com.example.daemawiki.domain.auth.dto.request.ReissueRequest;
+import com.example.daemawiki.domain.auth.dto.response.TokenResponse;
 import com.example.daemawiki.global.exception.H400.InvalidTokenException;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
