a6cf3de65ff3d26d6b916dadbfed332440745ae1
seungwon_H <tmddnjs001133@dsm.hs.kr>
2024-02-05 19:08:00 +0900

    리팩토링

--- src/main/java/com/example/daemawiki/domain/document/service/CreateDocument.java
+++ src/main/java/com/example/daemawiki/domain/document/service/CreateDocument.java
@@ -4,6 +4,7 @@ import com.example.daemawiki.domain.document.dto.request.CreateDocumentRequest;
 import com.example.daemawiki.domain.document.model.DefaultDocument;
 import com.example.daemawiki.domain.document.model.DocumentEditor;
 import com.example.daemawiki.domain.document.model.type.DocumentType;
+import com.example.daemawiki.domain.document.model.type.service.GetDocumentType;
 import com.example.daemawiki.domain.document.repository.DocumentRepository;
 import com.example.daemawiki.domain.revision.dto.SaveRevisionHistoryRequest;
 import com.example.daemawiki.domain.revision.model.type.RevisionType;
@@ -20,26 +21,21 @@ public class CreateDocument {
     private final UserFacade userFacade;
     private final DateTimeFacade dateTimeFacade;
     private final RevisionComponent revisionComponent;
+    private final GetDocumentType getDocumentType;
 
-    public CreateDocument(DocumentRepository documentRepository, UserFacade userFacade, DateTimeFacade dateTimeFacade, RevisionComponent revisionComponent) {
+    public CreateDocument(DocumentRepository documentRepository, UserFacade userFacade, DateTimeFacade dateTimeFacade, RevisionComponent revisionComponent, GetDocumentType getDocumentType) {
         this.documentRepository = documentRepository;
         this.userFacade = userFacade;
         this.dateTimeFacade = dateTimeFacade;
         this.revisionComponent = revisionComponent;
+        this.getDocumentType = getDocumentType;
     }
 
     public Mono<Void> execute(CreateDocumentRequest request) {
         return userFacade.currentUser()
                 .zipWith(dateTimeFacade.getKor(), (user, now) -> DefaultDocument.builder()
                             .title(request.title())
-                            .type(switch (request.type()) {
-                                case "student" -> DocumentType.STUDENT;
-                                case "teacher" -> DocumentType.TEACHER;
-                                case "club" -> DocumentType.CLUB;
-                                case "gen" -> DocumentType.GEN;
-                                case "major" -> DocumentType.MAJOR;
-                                case null, default -> DocumentType.DEFAULT;
-                            })
+                            .type(getDocumentType.execute(request.type()))
                             .dateTime(EditDateTime.builder()
                                     .created(now)
                                     .updated(now)
