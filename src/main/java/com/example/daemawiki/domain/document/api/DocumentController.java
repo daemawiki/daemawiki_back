8de6616e69a5baad704f7202f96eed43a828124b
seungwon_H <tmddnjs001133@dsm.hs.kr>
2024-02-05 20:30:55 +0900

    api 추가

--- src/main/java/com/example/daemawiki/domain/document/api/DocumentController.java
+++ src/main/java/com/example/daemawiki/domain/document/api/DocumentController.java
@@ -1,10 +1,12 @@
 package com.example.daemawiki.domain.document.api;
 
 import com.example.daemawiki.domain.document.dto.request.CreateDocumentRequest;
+import com.example.daemawiki.domain.document.dto.request.UpdateDocumentRequest;
 import com.example.daemawiki.domain.document.dto.response.GetDocumentResponse;
 import com.example.daemawiki.domain.document.service.CreateDocument;
 import com.example.daemawiki.domain.document.service.DeleteDocument;
 import com.example.daemawiki.domain.document.service.GetDocument;
+import com.example.daemawiki.domain.document.service.UpdateDocument;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,11 +18,13 @@ public class DocumentController {
     private final CreateDocument createDocumentService;
     private final GetDocument getDocumentService;
     private final DeleteDocument deleteDocumentService;
+    private final UpdateDocument updateDocumentService;
 
-    public DocumentController(CreateDocument createDocument, GetDocument getDocument, DeleteDocument deleteDocument) {
+    public DocumentController(CreateDocument createDocument, GetDocument getDocument, DeleteDocument deleteDocument, UpdateDocument updateDocument) {
         this.createDocumentService = createDocument;
         this.getDocumentService = getDocument;
         this.deleteDocumentService = deleteDocument;
+        this.updateDocumentService = updateDocument;
     }
 
     @PostMapping
@@ -37,6 +41,11 @@ public class DocumentController {
     @DeleteMapping("/{documentId}")
     public Mono<Void> deleteDocument(@PathVariable String documentId) {
         return deleteDocumentService.execute(documentId);
+    }
+
+    @PatchMapping
+    public Mono<Void> updateDocument(@RequestBody UpdateDocumentRequest request) {
+        return updateDocumentService.execute(request);
     }
 
 }
