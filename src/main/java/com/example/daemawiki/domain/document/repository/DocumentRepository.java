57a13bc371b1370be7342164254a5e9e72828146
101 <tmddnjs001133@dsm.hs.kr>
2024-02-13 11:59:39 +0900

    검색 기능 추가
    대소문자 구분 X

--- src/main/java/com/example/daemawiki/domain/document/repository/DocumentRepository.java
+++ src/main/java/com/example/daemawiki/domain/document/repository/DocumentRepository.java
@@ -1,6 +1,7 @@
 package com.example.daemawiki.domain.document.repository;
 
 import com.example.daemawiki.domain.document.model.DefaultDocument;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -8,6 +9,7 @@ import reactor.core.publisher.Mono;
 public interface DocumentRepository extends ReactiveMongoRepository<DefaultDocument, String> {
     Mono<DefaultDocument> findByTitle(String title);
 
-    Flux<DefaultDocument> findAllByGroupsContains(String group);
+    @Query("{'$or':[{'title':{$regex:'?0', $options:'i'}}, {'content':{$regex:'?0', $options:'i'}}]}")
+    Flux<DefaultDocument> findByTextContaining(String text);
 
 }
