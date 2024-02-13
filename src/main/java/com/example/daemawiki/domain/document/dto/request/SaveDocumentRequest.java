49d502e6e98ec553f82491d0d23b8ef4d5de13c3
101 <tmddnjs001133@dsm.hs.kr>
2024-02-14 05:40:26 +0900

    validation 추가

--- src/main/java/com/example/daemawiki/domain/document/dto/request/SaveDocumentRequest.java
+++ src/main/java/com/example/daemawiki/domain/document/dto/request/SaveDocumentRequest.java
@@ -1,12 +1,21 @@
 package com.example.daemawiki.domain.document.dto.request;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record SaveDocumentRequest(
+        @NotBlank(message = "문서 제목이 비어있습니다.")
         String title,
+        @NotBlank(message = "문서의 유형이 비어있습니다.")
         String type,
+        @NotNull(message = "문서의 내용이 null입니다.")
         String content,
+        @NotBlank(message = "문서의 분류가 비어있습니다.")
         List<List<String>> groups,
+        @Nullable
         Integer version
 ) {
 }
