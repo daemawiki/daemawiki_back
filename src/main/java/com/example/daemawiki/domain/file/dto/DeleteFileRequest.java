d90946007728a3f5fd3193b1da5caf0b0aa52d48
101 <tmddnjs001133@dsm.hs.kr>
2024-02-14 19:02:10 +0900

    리팩토링
    validation message 추가

--- src/main/java/com/example/daemawiki/domain/file/dto/DeleteFileRequest.java
+++ src/main/java/com/example/daemawiki/domain/file/dto/DeleteFileRequest.java
@@ -3,7 +3,7 @@ package com.example.daemawiki.domain.file.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record DeleteFileRequest(
-        @NotBlank
+        @NotBlank(message = "삭제할 파일의 이름을 작성해주세요.")
         String fileName
 ) {
 }
