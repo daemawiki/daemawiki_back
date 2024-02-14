fbf477901fe1d264bca85cfe71ff6118842dd981
101 <tmddnjs001133@dsm.hs.kr>
2024-02-14 19:00:38 +0900

    롬복 제거
    의존성 주입 내가 작성

--- src/main/java/com/example/daemawiki/domain/file/api/FileController.java
+++ src/main/java/com/example/daemawiki/domain/file/api/FileController.java
@@ -3,7 +3,6 @@ package com.example.daemawiki.domain.file.api;
 import com.example.daemawiki.domain.file.dto.DeleteFileRequest;
 import com.example.daemawiki.infra.s3.S3Service;
 import com.example.daemawiki.infra.s3.model.FileResponse;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
@@ -11,9 +10,12 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/image")
-@RequiredArgsConstructor
 public class FileController {
     private final S3Service service;
+
+    public FileController(S3Service service) {
+        this.service = service;
+    }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public Mono<FileResponse> uploadFile(@RequestPart("file") FilePart filePart, @RequestParam("type") String imageType) {
