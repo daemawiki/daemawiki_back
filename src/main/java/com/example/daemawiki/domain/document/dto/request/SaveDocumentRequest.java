c25d266bdfeb357d410a6234ea5fd17613923adc
101 <tmddnjs001133@dsm.hs.kr>
2024-02-15 21:59:34 +0900

    에러 수정

--- src/main/java/com/example/daemawiki/domain/document/dto/request/SaveDocumentRequest.java
+++ src/main/java/com/example/daemawiki/domain/document/dto/request/SaveDocumentRequest.java
@@ -11,9 +11,9 @@ public record SaveDocumentRequest(
         String title,
         @NotBlank(message = "문서의 유형이 비어있습니다.")
         String type,
-        @NotNull(message = "문서의 내용이 null입니다.")
+        @NotBlank(message = "문서의 내용이 비어있습니다.")
         String content,
-        @NotBlank(message = "문서의 분류가 비어있습니다.")
+        @NotNull(message = "문서의 분류가 비어있습니다.")
         List<List<String>> groups,
         @Nullable
         Integer version
