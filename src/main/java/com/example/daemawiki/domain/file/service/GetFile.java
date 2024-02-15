f251ef24d1ba75a646e5e31e95e86a3b18200396
101 <tmddnjs001133@dsm.hs.kr>
2024-02-15 13:51:42 +0900

    get file 추가

---
+++ src/main/java/com/example/daemawiki/domain/file/service/GetFile.java
@@ -0,0 +1,22 @@
+package com.example.daemawiki.domain.file.service;
+
+import com.example.daemawiki.domain.file.model.File;
+import com.example.daemawiki.domain.file.repository.FileRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@Service
+public class GetFile {
+    private final FileRepository fileRepository;
+
+    public GetFile(FileRepository fileRepository) {
+        this.fileRepository = fileRepository;
+    }
+
+    public Mono<File> getFileById(String id) {
+        return fileRepository.findById(UUID.fromString(id));
+    }
+
+}
