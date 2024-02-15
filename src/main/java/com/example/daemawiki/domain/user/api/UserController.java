7843075f6f240e4005cf20f291c606f4eb3fdcc9
101 <tmddnjs001133@dsm.hs.kr>
2024-02-15 14:25:19 +0900

    user controller 추가

--- src/main/java/com/example/daemawiki/domain/user/api/UserController.java
+++ src/main/java/com/example/daemawiki/domain/user/api/UserController.java
@@ -1,2 +1,28 @@
-package com.example.daemawiki.domain.user.api;public class UserController {
+package com.example.daemawiki.domain.user.api;
+
+import com.example.daemawiki.domain.user.service.ProfileImageUpload;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+/*
+
+    비밀번호 변경
+    프로필 이미지 변경
+    ...
+*/
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final ProfileImageUpload profileImageUpload;
+
+    public UserController(ProfileImageUpload profileImageUpload) {
+        this.profileImageUpload = profileImageUpload;
+    }
+
+    @PatchMapping("/profile-image")
+    public Mono<Void> profileUpload(@RequestPart FilePart filePart) {
+        return profileImageUpload.execute(filePart);
+    }
+
 }
