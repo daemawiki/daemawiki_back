699c64e33229dd5e5841c6753382e5498709a7b4
101 <tmddnjs001133@dsm.hs.kr>
2024-02-16 10:45:49 +0900

    비밀번호 변경 API 추가

--- src/main/java/com/example/daemawiki/domain/user/api/UserController.java
+++ src/main/java/com/example/daemawiki/domain/user/api/UserController.java
@@ -1,5 +1,7 @@
 package com.example.daemawiki.domain.user.api;
 
+import com.example.daemawiki.domain.user.dto.ChangePasswordRequest;
+import com.example.daemawiki.domain.user.service.ChangePassword;
 import com.example.daemawiki.domain.user.service.ProfileImageUpload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.multipart.FilePart;
@@ -16,15 +18,23 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/users")
 public class UserController {
     private final ProfileImageUpload profileImageUpload;
+    private final ChangePassword changePasswordService;
 
-    public UserController(ProfileImageUpload profileImageUpload) {
+    public UserController(ProfileImageUpload profileImageUpload, ChangePassword changePasswordService) {
         this.profileImageUpload = profileImageUpload;
+        this.changePasswordService = changePasswordService;
     }
 
     @PatchMapping("/profile-image")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> profileUpload(@RequestPart FilePart filePart) {
         return profileImageUpload.execute(filePart);
+    }
+
+    @PatchMapping("/password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> changePassword(@RequestBody ChangePasswordRequest request) {
+        return changePasswordService.execute(request);
     }
 
 }
