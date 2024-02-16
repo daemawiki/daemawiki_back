44f5c8b2afb6cd568a7430b218bf5142f611cdc0
101 <tmddnjs001133@dsm.hs.kr>
2024-02-16 10:39:37 +0900

    custom exception 처리

--- src/main/java/com/example/daemawiki/domain/user/service/ChangePassword.java
+++ src/main/java/com/example/daemawiki/domain/user/service/ChangePassword.java
@@ -3,6 +3,7 @@ package com.example.daemawiki.domain.user.service;
 import com.example.daemawiki.domain.user.dto.ChangePasswordRequest;
 import com.example.daemawiki.domain.user.repository.UserRepository;
 import com.example.daemawiki.domain.user.service.facade.UserFacade;
+import com.example.daemawiki.global.exception.h401.PasswordMismatchException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -22,7 +23,7 @@ public class ChangePassword {
     public Mono<Void> execute(ChangePasswordRequest request) {
         return userFacade.currentUser()
                 .filter(user -> passwordEncoder.matches(request.oldPassword(), user.getPassword()))
-                .switchIfEmpty(Mono.error(new RuntimeException()))
+                .switchIfEmpty(Mono.error(PasswordMismatchException.EXCEPTION))
                 .map(user -> {
                     user.changePassword(passwordEncoder.encode(request.newPassword()));
                     return user;
