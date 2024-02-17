59a89df6d6905c84d5702b45f570a6e879576e12
101 <tmddnjs001133@dsm.hs.kr>
2024-02-18 00:12:39 +0900

    기본 프로필 이미지 저장

--- src/main/java/com/example/daemawiki/domain/file/model/DefaultProfile.java
+++ src/main/java/com/example/daemawiki/domain/file/model/DefaultProfile.java
@@ -3,19 +3,32 @@ package com.example.daemawiki.domain.file.model;
 import com.example.daemawiki.domain.file.model.type.FileType;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.UUID;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultProfile {
-    private static final UUID id = UUID.randomUUID();
+
+    @Value("${profile.image.url}")
+    private static String defaultImageURL;
+
+    @Value("${profile.image.id}")
+    private static UUID defaultImageId;
+
+    @Value("${profile.image.name}")
+    private static String defaultImageName;
+
+    @Value("${profile.image.type}")
+    private static String defaultImageType;
+
     public static final File DEFAULT_PROFILE = File.builder()
-            .id(id)
-            .fileName("")
-            .fileType("")
+            .id(defaultImageId)
+            .fileName(defaultImageName)
+            .fileType(defaultImageType)
             .detail(FileDetail.builder()
                     .type(FileType.PROFILE)
-                    .url("")
+                    .url(defaultImageURL)
                     .build())
             .build();
 
