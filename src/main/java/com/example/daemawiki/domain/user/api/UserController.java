ae371d78c26fe1680cf9510a2c6efd91edba91c1
101 <tmddnjs001133@dsm.hs.kr>
2024-02-18 15:32:13 +0900

    API 추가

--- src/main/java/com/example/daemawiki/domain/user/api/UserController.java
+++ src/main/java/com/example/daemawiki/domain/user/api/UserController.java
@@ -1,11 +1,15 @@
 package com.example.daemawiki.domain.user.api;
 
 import com.example.daemawiki.domain.user.dto.ChangePasswordRequest;
+import com.example.daemawiki.domain.user.dto.GetUserResponse;
 import com.example.daemawiki.domain.user.service.ChangePassword;
+import com.example.daemawiki.domain.user.service.GetUser;
 import com.example.daemawiki.domain.user.service.ProfileImageUpload;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /*
@@ -19,10 +23,12 @@ import reactor.core.publisher.Mono;
 public class UserController {
     private final ProfileImageUpload profileImageUpload;
     private final ChangePassword changePasswordService;
+    private final GetUser getUser;
 
-    public UserController(ProfileImageUpload profileImageUpload, ChangePassword changePasswordService) {
+    public UserController(ProfileImageUpload profileImageUpload, ChangePassword changePasswordService, GetUser getUser) {
         this.profileImageUpload = profileImageUpload;
         this.changePasswordService = changePasswordService;
+        this.getUser = getUser;
     }
 
     @PatchMapping("/profile-image")
@@ -35,6 +41,16 @@ public class UserController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> changePassword(@RequestBody ChangePasswordRequest request) {
         return changePasswordService.execute(request);
+    }
+
+    @GetMapping
+    public Flux<GetUserResponse> getUserByGen(@NotNull @RequestParam Integer gen) {
+        return getUser.getUserByGen(gen);
+    }
+
+    @GetMapping
+    public Flux<GetUserResponse> getUserByMajor(@NotNull @RequestParam String major) {
+        return getUser.getUserByMajor(major);
     }
 
 }
