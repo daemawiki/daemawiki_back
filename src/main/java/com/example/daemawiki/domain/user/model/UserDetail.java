68ae599c976a1ce81856a2821a61ea65a7658c3f
101 <tmddnjs001133@dsm.hs.kr>
2024-02-18 23:29:24 +0900

    레코드에서 일반 클래스로 변경

--- src/main/java/com/example/daemawiki/domain/user/model/UserDetail.java
+++ src/main/java/com/example/daemawiki/domain/user/model/UserDetail.java
@@ -2,10 +2,18 @@ package com.example.daemawiki.domain.user.model;
 
 import com.example.daemawiki.domain.user.model.type.major.MajorType;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @Builder
-public record UserDetail(
-        Integer gen,
-        MajorType major
-) {
+public class UserDetail{
+
+    private Integer gen;
+
+    private MajorType major;
+
+    private String club;
+
 }
