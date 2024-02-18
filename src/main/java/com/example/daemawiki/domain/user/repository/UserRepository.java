fa6a6f96c74fa6cd9c50de7097d4dace9f573cdc
101 <tmddnjs001133@dsm.hs.kr>
2024-02-18 15:28:06 +0900

    기수, 전공으로 학생 찾기

--- src/main/java/com/example/daemawiki/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/daemawiki/domain/user/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package com.example.daemawiki.domain.user.repository;
 
 import com.example.daemawiki.domain.user.model.User;
+import com.example.daemawiki.domain.user.model.type.MajorType;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     Mono<User> findByEmail(String email);
+
+    Flux<User> findAllByGen(Integer gen);
+
+    Flux<User> findAllByMajor(MajorType major);
 }
