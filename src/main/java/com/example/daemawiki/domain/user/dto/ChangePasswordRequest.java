e145f804571d45a50077268386f713614e72e443
101 <tmddnjs001133@dsm.hs.kr>
2024-02-22 13:02:03 +0900

    비밀번호 req에 email 필드 추가

--- src/main/java/com/example/daemawiki/domain/user/dto/ChangePasswordRequest.java
+++ src/main/java/com/example/daemawiki/domain/user/dto/ChangePasswordRequest.java
@@ -1,10 +1,13 @@
 package com.example.daemawiki.domain.user.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 
 public record ChangePasswordRequest(
         //String oldPassword,
         @NotBlank
-        String newPassword
+        String newPassword,
+        @Nullable
+        String email
 ) {
 }
