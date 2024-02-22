c1986d0acc3023ec19c18b6161e380afff57f642
101 <tmddnjs001133@dsm.hs.kr>
2024-02-23 08:07:10 +0900

    JsonFormat 추가

--- src/main/java/com/example/daemawiki/domain/revision/dto/response/RevisionDocumentDetailResponse.java
+++ src/main/java/com/example/daemawiki/domain/revision/dto/response/RevisionDocumentDetailResponse.java
@@ -1,5 +1,6 @@
 package com.example.daemawiki.domain.revision.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ public record RevisionDocumentDetailResponse(
         String documentId,
         String title,
         Integer numberOfRevision,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
         LocalDateTime updatedDate
 ) {
 }
