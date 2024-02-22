04e92be853a0a2ff475a34c7c40af5e577318ac9
101 <tmddnjs001133@dsm.hs.kr>
2024-02-22 13:01:12 +0900

    메일 전송 req에 type 추가

--- src/main/java/com/example/daemawiki/domain/mail/dto/AuthCodeRequest.java
+++ src/main/java/com/example/daemawiki/domain/mail/dto/AuthCodeRequest.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.Pattern;
 public record AuthCodeRequest(
         @Pattern(regexp = "^[A-Za-z0-9+_.-]+@(dsm\\.hs\\.kr)$",
                 message = "메일 형식이 올바르지 않습니다.")
-        String mail
+        String mail,
+
+        String type
 ) {
 }
