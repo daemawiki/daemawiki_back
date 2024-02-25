4da7595b638a6df8037f9350f2eb10a82cf195e0
101 <tmddnjs001133@dsm.hs.kr>
2024-02-25 21:51:39 +0900

    응답 시간 개선

--- src/main/java/com/example/daemawiki/domain/user/service/UpdateClub.java
+++ src/main/java/com/example/daemawiki/domain/user/service/UpdateClub.java
@@ -1,14 +1,14 @@
 package com.example.daemawiki.domain.user.service;
 
 import com.example.daemawiki.domain.document.component.facade.DocumentFacade;
-import com.example.daemawiki.domain.document.model.DefaultDocument;
 import com.example.daemawiki.domain.document.repository.DocumentRepository;
 import com.example.daemawiki.domain.user.dto.UpdateClubRequest;
-import com.example.daemawiki.domain.user.model.User;
 import com.example.daemawiki.domain.user.repository.UserRepository;
 import com.example.daemawiki.domain.user.service.facade.UserFacade;
+import com.example.daemawiki.global.exception.h500.ExecuteFailedException;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 
 import java.util.Arrays;
 
@@ -18,25 +18,26 @@ public class UpdateClub {
     private final DocumentFacade documentFacade;
     private final UserRepository userRepository;
     private final DocumentRepository documentRepository;
+    private final Scheduler scheduler;
 
-    public UpdateClub(UserFacade userFacade, DocumentFacade documentFacade, UserRepository userRepository, DocumentRepository documentRepository) {
+    public UpdateClub(UserFacade userFacade, DocumentFacade documentFacade, UserRepository userRepository, DocumentRepository documentRepository, Scheduler scheduler) {
         this.userFacade = userFacade;
         this.documentFacade = documentFacade;
         this.userRepository = userRepository;
         this.documentRepository = documentRepository;
+        this.scheduler = scheduler;
     }
 
     public Mono<Void> execute(UpdateClubRequest request) {
-        Mono<User> userMono = userFacade.currentUser()
+        return userFacade.currentUser()
                 .doOnNext(user -> user.getDetail().setClub(request.club()))
-                .flatMap(userRepository::save);
-
-        Mono<DefaultDocument> documentMono = userMono
-                .flatMap(user -> documentFacade.findDocumentById(user.getDocumentId()))
-                .doOnNext(document -> document.getGroups().add(Arrays.asList("동아리", request.club())))
-                .flatMap(documentRepository::save);
-
-        return Mono.when(userMono, documentMono).then();
+                .flatMap(userRepository::save)
+                .flatMap(user -> documentFacade.findDocumentById(user.getDocumentId())
+                        .doOnNext(document -> document.getGroups().add(Arrays.asList("동아리", request.club())))
+                        .flatMap(documentRepository::save))
+                .subscribeOn(scheduler)
+                .onErrorMap(e -> ExecuteFailedException.EXCEPTION)
+                .then();
     }
-    
+
 }
