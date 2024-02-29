ddd28e6d93de4abe914dd14fdf9b255519d1eb44
101 <tmddnjs001133@dsm.hs.kr>
2024-02-29 13:24:07 +0900

    리팩토링

---
+++ src/main/java/com/example/daemawiki/domain/content/api/DocumentContentController.java
@@ -0,0 +1,34 @@
+package com.example.daemawiki.domain.content.api;
+
+import com.example.daemawiki.domain.content.dto.AddContentRequest;
+import com.example.daemawiki.domain.content.dto.WriteContentRequest;
+import com.example.daemawiki.domain.content.service.AddContentTable;
+import com.example.daemawiki.domain.content.service.WriteContent;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/documents/content")
+public class DocumentContentController {
+    private final WriteContent writeContentService;
+    private final AddContentTable addContentTableService;
+
+    public DocumentContentController(WriteContent writeContentService, AddContentTable addContentTableService) {
+        this.writeContentService = writeContentService;
+        this.addContentTableService = addContentTableService;
+    }
+
+    @PatchMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> writeContent(@RequestBody WriteContentRequest request) {
+        return writeContentService.execute(request);
+    }
+
+    @PatchMapping("/table")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> addContentTable(@RequestBody AddContentRequest request) {
+        return addContentTableService.execute(request);
+    }
+
+}
--- src/main/java/com/example/daemawiki/domain/document/api/DocumentController.java
+++ src/main/java/com/example/daemawiki/domain/document/api/DocumentController.java
@@ -1,9 +1,5 @@
 package com.example.daemawiki.domain.document.api;
 
-import com.example.daemawiki.domain.content.dto.AddContentRequest;
-import com.example.daemawiki.domain.content.dto.WriteContentRequest;
-import com.example.daemawiki.domain.content.service.AddContentTable;
-import com.example.daemawiki.domain.content.service.WriteContent;
 import com.example.daemawiki.domain.document.component.service.CreateDocument;
 import com.example.daemawiki.domain.document.component.service.DeleteDocument;
 import com.example.daemawiki.domain.document.component.service.GetDocument;
@@ -12,7 +8,6 @@ import com.example.daemawiki.domain.document.dto.request.SaveDocumentRequest;
 import com.example.daemawiki.domain.document.dto.response.GetDocumentResponse;
 import com.example.daemawiki.domain.document.dto.response.SearchDocumentResponse;
 import com.example.daemawiki.domain.document.dto.response.SimpleDocumentResponse;
-import com.example.daemawiki.domain.editor.service.AddEditor;
 import com.example.daemawiki.domain.info.dto.UpdateInfoRequest;
 import com.example.daemawiki.domain.info.service.UpdateInfo;
 import jakarta.validation.Valid;
@@ -30,19 +25,13 @@ public class DocumentController {
     private final DeleteDocument deleteDocumentService;
     private final UpdateDocument updateDocumentService;
     private final UpdateInfo updateInfoService;
-    private final WriteContent writeContentService;
-    private final AddContentTable addContentTableService;
-    private final AddEditor addEditorService;
 
-    public DocumentController(CreateDocument createDocument, GetDocument getDocument, DeleteDocument deleteDocument, UpdateDocument updateDocument, UpdateInfo updateInfoService, WriteContent writeContentService, AddContentTable addContentTableService, AddEditor addEditorService) {
+    public DocumentController(CreateDocument createDocument, GetDocument getDocument, DeleteDocument deleteDocument, UpdateDocument updateDocument, UpdateInfo updateInfoService) {
         this.createDocumentService = createDocument;
         this.getDocumentService = getDocument;
         this.deleteDocumentService = deleteDocument;
         this.updateDocumentService = updateDocument;
         this.updateInfoService = updateInfoService;
-        this.writeContentService = writeContentService;
-        this.addContentTableService = addContentTableService;
-        this.addEditorService = addEditorService;
     }
 
     @PostMapping
@@ -87,18 +76,6 @@ public class DocumentController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> updateInfo(@RequestBody UpdateInfoRequest request) {
         return updateInfoService.execute(request);
-    }
-
-    @PatchMapping("/content")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Mono<Void> writeContent(@RequestBody WriteContentRequest request) {
-        return writeContentService.execute(request);
-    }
-
-    @PatchMapping("/content/table")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public Mono<Void> addContentTable(@RequestBody AddContentRequest request) {
-        return addContentTableService.execute(request);
     }
 
 }
