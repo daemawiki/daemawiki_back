18460b04526a6ad68388668ff10fc55e8af8ced7
101 <tmddnjs001133@dsm.hs.kr>
2024-03-09 22:02:43 +0900

    리팩토링

--- src/main/java/com/example/daemawiki/domain/content/service/AddContentTable.java
+++ src/main/java/com/example/daemawiki/domain/content/service/AddContentTable.java
@@ -94,11 +94,8 @@ public class AddContentTable {
     }
 
     private Mono<Void> createRevision(DefaultDocument document) {
-        return revisionComponent.saveHistory(SaveRevisionHistoryRequest.builder()
-                .type(RevisionType.UPDATE)
-                .documentId(document.getId())
-                .title(document.getTitle())
-                .build());
+        return revisionComponent.saveHistory(SaveRevisionHistoryRequest
+                .create(RevisionType.UPDATE, document.getId(), document.getTitle()));
     }
 
     private Throwable mapException(Throwable e) {
