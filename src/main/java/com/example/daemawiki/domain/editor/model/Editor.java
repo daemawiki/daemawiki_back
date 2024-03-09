24f33671249afcaeae67d988420b695541c96cf7
101 <tmddnjs001133@dsm.hs.kr>
2024-03-09 21:50:53 +0900

    리팩토링

--- src/main/java/com/example/daemawiki/domain/editor/model/Editor.java
+++ src/main/java/com/example/daemawiki/domain/editor/model/Editor.java
@@ -4,4 +4,12 @@ import lombok.Builder;
 
 @Builder
 public record Editor(String user, String id) {
+
+    public static Editor createEditor(String user, String id) {
+        return Editor.builder()
+                .user(user)
+                .id(id)
+                .build();
+    }
+
 }
