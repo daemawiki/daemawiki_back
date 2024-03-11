fd16ffcb2d7ea7177cef582975db6d2669a738b6
101 <tmddnjs001133@dsm.hs.kr>
2024-03-11 17:06:21 +0900

    add UpdataDocumentEditorAndUpdatedDate.java
    for refactoring

---
+++ src/main/java/com/example/daemawiki/domain/document/component/UpdateDocumentEditorAndUpdatedDate.java
@@ -0,0 +1,18 @@
+package com.example.daemawiki.domain.document.component;
+
+import com.example.daemawiki.domain.document.model.DefaultDocument;
+import com.example.daemawiki.domain.user.dto.response.UserDetailResponse;
+import com.example.daemawiki.domain.user.model.User;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class UpdateDocumentEditorAndUpdatedDate {
+
+    public void execute(DefaultDocument document, User user) {
+        document.getEditor().setUpdatedUser(UserDetailResponse.create(user));
+        document.getDateTime().setUpdated(LocalDateTime.now());
+    }
+
+}
