e2515864564bb624a0b74efc305d07ed48f54551
101 <tmddnjs001133@dsm.hs.kr>
2024-03-11 11:36:16 +0900

    update CreateDocumentFacade.java
    add Default Info builder

--- src/main/java/com/example/daemawiki/domain/document/component/facade/CreateDocumentFacade.java
+++ src/main/java/com/example/daemawiki/domain/document/component/facade/CreateDocumentFacade.java
@@ -5,9 +5,12 @@ import com.example.daemawiki.domain.document.model.DefaultDocument;
 import com.example.daemawiki.domain.document.model.type.service.GetDocumentType;
 import com.example.daemawiki.domain.editor.model.DocumentEditor;
 import com.example.daemawiki.domain.editor.model.Editor;
+import com.example.daemawiki.domain.file.model.DefaultProfile;
+import com.example.daemawiki.domain.info.model.Info;
 import com.example.daemawiki.domain.user.dto.response.UserDetailResponse;
 import com.example.daemawiki.domain.user.model.User;
 import com.example.daemawiki.global.datetime.model.EditDateTime;
+import org.eclipse.collections.api.factory.Lists;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -16,9 +19,11 @@ import java.util.Collections;
 @Component
 public class CreateDocumentFacade {
     private final GetDocumentType getDocumentType;
+    private final DefaultProfile defaultProfile;
 
-    public CreateDocumentFacade(GetDocumentType getDocumentType) {
+    public CreateDocumentFacade(GetDocumentType getDocumentType, DefaultProfile defaultProfile) {
         this.getDocumentType = getDocumentType;
+        this.defaultProfile = defaultProfile;
     }
 
     public DefaultDocument execute(SaveDocumentRequest request, User user) {
@@ -30,6 +35,11 @@ public class CreateDocumentFacade {
                 .dateTime(EditDateTime.builder()
                         .created(LocalDateTime.now())
                         .updated(LocalDateTime.now())
+                        .build())
+                .info(Info.builder()
+                        .documentImage(defaultProfile.defaultDocumentImage())
+                        .subTitle("")
+                        .details(Lists.mutable.of())
                         .build())
                 .documentEditor(DocumentEditor.builder()
                         .createdUser(userDetail)
