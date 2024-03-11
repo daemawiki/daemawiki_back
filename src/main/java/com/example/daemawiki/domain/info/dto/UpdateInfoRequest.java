73338dddc445112b6849d40dab9374b79ab66332
101 <tmddnjs001133@dsm.hs.kr>
2024-03-12 08:43:46 +0900

    update UpdateInfoRequest.java
    add not null annotation

--- src/main/java/com/example/daemawiki/domain/info/dto/UpdateInfoRequest.java
+++ src/main/java/com/example/daemawiki/domain/info/dto/UpdateInfoRequest.java
@@ -9,6 +9,7 @@ import java.util.List;
 public record UpdateInfoRequest(
         @NotBlank(message = "문서의 id를 입력해주세요.")
         String documentId,
+        @NotNull(message = "subTitle이 null입니다.")
         String subTitle,
         List<Detail> details,
         @NotNull(message = "문서의 버전을 입력해주세요.")
