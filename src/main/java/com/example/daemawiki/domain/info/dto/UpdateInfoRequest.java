33c4aeb44d4e4d5c1eb08042575b722de07a13ab
101 <tmddnjs001133@dsm.hs.kr>
2024-03-11 11:35:10 +0900

    update UpdateInfoRequest.java
    add subTitle field

--- src/main/java/com/example/daemawiki/domain/info/dto/UpdateInfoRequest.java
+++ src/main/java/com/example/daemawiki/domain/info/dto/UpdateInfoRequest.java
@@ -1,6 +1,6 @@
 package com.example.daemawiki.domain.info.dto;
 
-import com.example.daemawiki.domain.info.model.Info;
+import com.example.daemawiki.domain.info.model.Detail;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -9,7 +9,8 @@ import java.util.List;
 public record UpdateInfoRequest(
         @NotBlank(message = "문서의 id를 입력해주세요.")
         String documentId,
-        List<Info> infoList,
+        String subTitle,
+        List<Detail> details,
         @NotNull(message = "문서의 버전을 입력해주세요.")
         Integer version
 ) {
