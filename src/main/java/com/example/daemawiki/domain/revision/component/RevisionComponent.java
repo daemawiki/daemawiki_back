62e3cb6c6aea0e882296192dc15b432edf054a89
101 <tmddnjs001133@dsm.hs.kr>
2024-03-15 21:38:30 +0900

    refactor RevisionComponent.java
    add javadoc

--- src/main/java/com/example/daemawiki/domain/revision/component/RevisionComponent.java
+++ src/main/java/com/example/daemawiki/domain/revision/component/RevisionComponent.java
@@ -1,9 +1,16 @@
 package com.example.daemawiki.domain.revision.component;
 
 import com.example.daemawiki.domain.revision.dto.request.SaveRevisionHistoryRequest;
+import com.example.daemawiki.global.exception.h500.ExecuteFailedException;
 import reactor.core.publisher.Mono;
 
 public interface RevisionComponent {
-    Mono<Void> saveHistory(SaveRevisionHistoryRequest request);
+    /**
+     *
+     * @param saveRevisionHistoryRequest
+     * @return void
+     * @exception ExecuteFailedException
+     */
+    Mono<Void> saveHistory(SaveRevisionHistoryRequest saveRevisionHistoryRequest);
 
 }
