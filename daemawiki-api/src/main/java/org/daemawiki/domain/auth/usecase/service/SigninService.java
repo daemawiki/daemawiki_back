81f8df79d1208839bdf722a6f87c7761ce8215d4
101 <tmddnjs001133@dsm.hs.kr>
2024-03-20 11:16:19 +0900

    refactor SigninService.java
    add javadoc

--- daemawiki-api/src/main/java/org/daemawiki/domain/auth/usecase/service/SigninService.java
+++ daemawiki-api/src/main/java/org/daemawiki/domain/auth/usecase/service/SigninService.java
@@ -24,20 +24,39 @@ public class SigninService implements SigninUsecase {
         this.tokenizer = tokenizer;
     }
 
+    /**
+     * 사용자 로그인 메서드
+     *
+     * @param request 로그인 요청 객체
+     * @return 토큰 응답 Mono
+     */
     @Override
     public Mono<TokenResponse> signin(LoginRequest request) {
         return getUserPort.findByEmail(request.email())
                 .switchIfEmpty(Mono.error(UserNotFoundException.EXCEPTION))
-                .flatMap(user -> validateUserAndPassword(user, request.password()))
+                .flatMap(user -> validateUserWithPassword(user, request.password()))
                 .flatMap(this::generateTokenResponse);
     }
 
+    /**
+     * 유저 토큰 생성
+     *
+     * @param user 토큰을 생성할 유저
+     * @return 토큰 응답 Mono
+     */
     private Mono<TokenResponse> generateTokenResponse(User user) {
         return tokenizer.createToken(user.getEmail())
                 .map(TokenResponse::create);
     }
 
-    private Mono<User> validateUserAndPassword(User user, String inputPassword) {
+    /**
+     * 사용자와 입력된 비밀번호를 검증.
+     *
+     * @param user 사용자 정보
+     * @param inputPassword 입력된 비밀번호
+     * @return Mono<User> 사용자 정보 Mono
+     */
+    private Mono<User> validateUserWithPassword(User user, String inputPassword) {
         return Mono.just(user)
                 .filter(u -> passwordEncoder.matches(inputPassword, u.getPassword()))
                 .switchIfEmpty(Mono.error(PasswordMismatchException.EXCEPTION));
