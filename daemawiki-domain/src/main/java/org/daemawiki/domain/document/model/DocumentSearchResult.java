0dff55d3a42ed4ee6427c667d35ac4caa42bd4b7
101 <tmddnjs001133@dsm.hs.kr>
2024-04-08 13:35:41 +0900

    update :: DocumentSearchResult.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/document/model/DocumentSearchResult.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/document/model/DocumentSearchResult.java
@@ -1,10 +1,12 @@
 package org.daemawiki.domain.document.model;
 
+import lombok.Builder;
 import org.daemawiki.datetime.model.EditDateTime;
 import org.daemawiki.domain.content.model.Content;
 import org.daemawiki.domain.document.model.type.DocumentType;
 import lombok.Data;
 
+@Builder
 @Data
 public class DocumentSearchResult {
     private String id;
