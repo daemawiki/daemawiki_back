3c5669d026b22e67417165486a4e578e2cc0d1ba
101 <tmddnjs001133@dsm.hs.kr>
2024-04-08 13:34:16 +0900

    update :: ReissueUsecase.java

--- daemawiki-service/src/main/java/org/daemawiki/domain/auth/usecase/ReissueUsecase.java
+++ daemawiki-service/src/main/java/org/daemawiki/domain/auth/usecase/ReissueUsecase.java
@@ -1,10 +1,9 @@
 package org.daemawiki.domain.auth.usecase;
 
-import org.daemawiki.domain.auth.dto.request.ReissueRequest;
 import org.daemawiki.domain.auth.dto.response.TokenResponse;
 import reactor.core.publisher.Mono;
 
 public interface ReissueUsecase {
-    Mono<TokenResponse> reissue(ReissueRequest request);
+    Mono<TokenResponse> reissue(String token);
 
 }
