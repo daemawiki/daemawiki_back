9701cc1260308ea8415243078526b8c5ee378df6
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 13:20:55 +0900

    add :: GetMostViewDocumentResponse.java

---
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/document/dto/response/GetMostViewDocumentResponse.java
@@ -0,0 +1,27 @@
+package org.daemawiki.domain.document.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import org.daemawiki.domain.document.model.DefaultDocument;
+
+import java.time.LocalDateTime;
+
+@Builder
+public record GetMostViewDocumentResponse(
+        String id,
+        String title,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
+        LocalDateTime updatedDate,
+        int view
+) {
+
+    public static GetMostViewDocumentResponse of(DefaultDocument document) {
+        return GetMostViewDocumentResponse.builder()
+                .id(document.getId())
+                .title(document.getTitle())
+                .updatedDate(document.getDateTime().getUpdated())
+                .view(document.getView())
+                .build();
+    }
+
+}
