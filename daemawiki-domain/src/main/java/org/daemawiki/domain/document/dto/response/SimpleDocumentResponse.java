ca9ea632f82d48a572238e2ef432fac4136779d1
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 20:22:06 +0900

    update :: SimpleDocumentResponse.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/document/dto/response/SimpleDocumentResponse.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/document/dto/response/SimpleDocumentResponse.java
@@ -10,10 +10,10 @@ import java.time.LocalDateTime;
 public record SimpleDocumentResponse(
         String id,
         String title,
-        Integer numberOfUpdate,
+        Long numberOfUpdate,
         @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "Asia/Seoul")
         LocalDateTime updatedDate,
-        Integer view
+        Long view
 ) {
 
         public static SimpleDocumentResponse of(DefaultDocument document) {
