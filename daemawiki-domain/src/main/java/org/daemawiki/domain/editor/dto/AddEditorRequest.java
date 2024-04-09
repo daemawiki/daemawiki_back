e2ec723a5059ca0dd52f5ed271e337b5a3889768
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 20:21:57 +0900

    update :: AddEditorRequest.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/editor/dto/AddEditorRequest.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/editor/dto/AddEditorRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record AddEditorRequest(
         @NotBlank(message = "문서 변경 권한을 줄 유저의 이메일을 입력해주세요.")
-        String email) {
+        String email
+) {
 }
