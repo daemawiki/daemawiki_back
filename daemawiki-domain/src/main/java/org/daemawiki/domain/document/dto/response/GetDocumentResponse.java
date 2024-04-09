98d8e82b0942fb6c8fa40197a595d8940e48dc8c
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 13:20:41 +0900

    update :: GetDocumentResponse.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/document/dto/response/GetDocumentResponse.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/document/dto/response/GetDocumentResponse.java
@@ -2,6 +2,7 @@ package org.daemawiki.domain.document.dto.response;
 
 import org.daemawiki.datetime.model.EditDateTime;
 import org.daemawiki.domain.content.model.Content;
+import org.daemawiki.domain.document.model.DefaultDocument;
 import org.daemawiki.domain.document.model.type.DocumentType;
 import org.daemawiki.domain.editor.model.DocumentEditor;
 import org.daemawiki.domain.info.model.Info;
@@ -20,7 +21,22 @@ public record GetDocumentResponse(
         DocumentEditor editor,
         List<Content> content,
         Integer view,
-
         Integer version
 ) {
+
+    public static GetDocumentResponse of(DefaultDocument document) {
+        return GetDocumentResponse.builder()
+                .id(document.getId())
+                .title(document.getTitle())
+                .type(document.getType())
+                .dateTime(document.getDateTime())
+                .info(document.getInfo())
+                .groups(document.getGroups())
+                .editor(document.getEditor())
+                .content(document.getContents())
+                .view(document.getView())
+                .version(document.getVersion())
+                .build();
+    }
+
 }
