311b26663b4da35ba007614513b52c434b8d0145
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 20:19:39 +0900

    update :: S3DeleteObject.java

--- daemawiki-service/src/main/java/org/daemawiki/infra/s3/service/S3DeleteObject.java
+++ daemawiki-service/src/main/java/org/daemawiki/infra/s3/service/S3DeleteObject.java
@@ -1,9 +1,8 @@
 package org.daemawiki.infra.s3.service;
 
-import org.daemawiki.domain.file.dto.DeleteFileRequest;
 import reactor.core.publisher.Mono;
 
 public interface S3DeleteObject {
-    Mono<Void> deleteObject(DeleteFileRequest request);
+    Mono<Void> deleteObject(String fileId);
 
 }
