32d58a3405a92e3259cbe2505f9ee42137f90079
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 20:21:05 +0900

    update :: DeleteEditorRequest.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/editor/dto/DeleteEditorRequest.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/editor/dto/DeleteEditorRequest.java
@@ -4,5 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record DeleteEditorRequest(
         @NotBlank(message = "문서 변경 권한을 삭제할 유저의 id를 입력해주세요.")
-        String userId) {
+        String userId
+) {
 }
