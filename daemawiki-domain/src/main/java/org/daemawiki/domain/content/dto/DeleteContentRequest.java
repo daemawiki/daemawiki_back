31ddd1a67837ab27fb305172cce07514fe43341b
101 <tmddnjs001133@dsm.hs.kr>
2024-04-09 20:23:27 +0900

    update :: DeleteContentRequest.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/content/dto/DeleteContentRequest.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/content/dto/DeleteContentRequest.java
@@ -7,6 +7,6 @@ public record DeleteContentRequest(
         @NotBlank(message = "추가할 목차의 인덱스를 입력해주세요.")
         String index,
         @NotNull(message = "문서의 버전을 입력해주세요.")
-        Integer version
+        Long version
 ) {
 }
