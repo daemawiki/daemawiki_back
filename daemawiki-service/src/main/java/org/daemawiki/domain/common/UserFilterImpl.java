221b1763f84cfe8cef6775184ae056f4ace09b5e
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 19:49:25 +0900

    update :: UserFilterImpl.java

--- daemawiki-service/src/main/java/org/daemawiki/domain/common/UserFilterImpl.java
+++ daemawiki-service/src/main/java/org/daemawiki/domain/common/UserFilterImpl.java
@@ -12,23 +12,27 @@ import java.util.Objects;
 public class UserFilterImpl implements UserFilter {
 
     @Override
-    public void userPermissionAndDocumentVersionCheck(DefaultDocument document, String userEmail, Long requestVersion) {
-        userPermissionCheck(document, userEmail);
-        if (!Objects.equals(document.getVersion(), requestVersion)) {
-            throw VersionMismatchException.EXCEPTION;
+    public void userPermissionAndDocumentVersionCheck(DefaultDocument document, User user, Long requestVersion) {
+        if (!user.getIsAdmin()) {
+            userPermissionCheck(document, user);
+            if (!Objects.equals(document.getVersion(), requestVersion)) {
+                throw VersionMismatchException.EXCEPTION;
+            }
         }
     }
 
     @Override
-    public void userPermissionCheck(DefaultDocument document, String userEmail) {
-        if (document.getEditor().hasEditPermission(userEmail)) {
-            throw NoEditPermissionUserException.EXCEPTION;
+    public void userPermissionCheck(DefaultDocument document, User user) {
+        if (!user.getIsAdmin()) {
+            if (document.getEditor().hasEditPermission(user.getEmail()) || user.getIsBlocked()) {
+                throw NoEditPermissionUserException.EXCEPTION;
+            }
         }
     }
 
     @Override
     public void checkUserAndDocument(User user, DefaultDocument document, Long version) {
-        userPermissionAndDocumentVersionCheck(document, user.getEmail(), version);
+        userPermissionAndDocumentVersionCheck(document, user, version);
     }
 
 }
