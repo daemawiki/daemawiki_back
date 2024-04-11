9013d408dc37d427908e4eba61fd414138d0b5d4
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 19:48:42 +0900

    update :: User.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/user/model/User.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/user/model/User.java
@@ -1,18 +1,16 @@
 package org.daemawiki.domain.user.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.daemawiki.domain.file.model.File;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.daemawiki.domain.file.model.File;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @Document
-@NoArgsConstructor
 public class User {
 
     @Id
@@ -31,14 +29,21 @@ public class User {
 
     private String documentId;
 
+    private Boolean isAdmin;
+
+    private Boolean isBlocked = false;
+
+    protected User() {}
+
     @Builder
-    public User(String name, String email, String password, File profile, UserDetail detail, String documentId) {
+    public User(String name, String email, String password, File profile, UserDetail detail, String documentId, Boolean isAdmin) {
         this.name = name;
         this.email = email;
         this.password = password;
         this.profile = profile;
         this.detail = detail;
         this.documentId = documentId;
+        this.isAdmin = isAdmin;
     }
 
     public void update(String name, UserDetail detail) {
