1e0d3f5a8a29e2208e0811859ff1cc0e7f080955
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 19:48:55 +0900

    refactor :: FindRevisionPort.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/revision/application/FindRevisionPort.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/revision/application/FindRevisionPort.java
@@ -3,7 +3,7 @@ package org.daemawiki.domain.revision.application;
 import org.daemawiki.domain.revision.model.RevisionHistory;
 import reactor.core.publisher.Flux;
 
-public interface GetRevisionPort {
+public interface FindRevisionPort {
     Flux<RevisionHistory> getUpdatedTop10Revision();
     Flux<RevisionHistory> getAllRevisionPaging(String lastRevisionId);
     Flux<RevisionHistory> getAllRevisionByDocument(String documentId, String lastRevisionId);
