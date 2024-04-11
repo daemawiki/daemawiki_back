53f3d40325b702a7c124c21eb14b4280e0b9810d
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 20:07:20 +0900

    update :: Tokenizer.java

--- daemawiki-auth/src/main/java/org/daemawiki/security/Tokenizer.java
+++ daemawiki-auth/src/main/java/org/daemawiki/security/Tokenizer.java
@@ -2,10 +2,13 @@ package org.daemawiki.security;
 
 import org.springframework.security.core.Authentication;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.LocalDateTime;
 
 public interface Tokenizer {
-    Mono<String> createToken(String user);
-    Mono<String> reissue(String token);
+    Mono<Tuple2<String, LocalDateTime>> createToken(String user);
+    Mono<Tuple2<String, LocalDateTime>> reissue(String token);
     Mono<Authentication> getAuthentication(String token);
 
 }
