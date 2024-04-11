a6af6f1146bfb95bd6c7d3d77606d6cc7766a97b
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 20:04:46 +0900

    refactor :: FindAuthMailPort.java

--- daemawiki-domain/src/main/java/org/daemawiki/domain/mail/application/mail/FindAuthMailPort.java
+++ daemawiki-domain/src/main/java/org/daemawiki/domain/mail/application/mail/FindAuthMailPort.java
@@ -2,7 +2,7 @@ package org.daemawiki.domain.mail.application.mail;
 
 import reactor.core.publisher.Mono;
 
-public interface GetAuthMailPort {
+public interface FindAuthMailPort {
     Mono<Boolean> findByMail(String mail);
 
 }
