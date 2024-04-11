7323e7ab50ce779004d022d032c83eec39d4190b
101 <tmddnjs001133@dsm.hs.kr>
2024-04-11 19:57:43 +0900

    refactor :: GetFileService.java

--- daemawiki-service/src/main/java/org/daemawiki/domain/file/service/GetFileService.java
+++ daemawiki-service/src/main/java/org/daemawiki/domain/file/service/GetFileService.java
@@ -1,6 +1,6 @@
-package org.daemawiki.domain.file.usecase.service;
+package org.daemawiki.domain.file.service;
 
-import org.daemawiki.domain.file.application.GetFilePort;
+import org.daemawiki.domain.file.application.FindFilePort;
 import org.daemawiki.domain.file.model.File;
 import org.daemawiki.domain.file.usecase.GetFileUsecase;
 import org.daemawiki.exception.h404.FileNotFoundException;
@@ -12,26 +12,26 @@ import java.util.UUID;
 
 @Service
 public class GetFileService implements GetFileUsecase {
-    private final GetFilePort getFilePort;
+    private final FindFilePort findFilePort;
 
-    public GetFileService(GetFilePort getFilePort) {
-        this.getFilePort = getFilePort;
+    public GetFileService(FindFilePort findFilePort) {
+        this.findFilePort = findFilePort;
     }
 
     @Override
     public Mono<File> getFileById(String id) {
-        return getFilePort.findById(UUID.fromString(id))
+        return findFilePort.findById(UUID.fromString(id))
                 .switchIfEmpty(Mono.error(FileNotFoundException.EXCEPTION));
     }
 
     @Override
     public Flux<File> getFileByName(String fileName) {
-        return getFilePort.findByName(fileName);
+        return findFilePort.findByName(fileName);
     }
 
     @Override
     public Flux<File> getAll() {
-        return getFilePort.findAll();
+        return findFilePort.findAll();
     }
 
 }
